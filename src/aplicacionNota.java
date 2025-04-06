bb82b694e8b8ce37402ec79c50d962d4137cd328
javi3rmtz <eljavier2006@gmail.com>
2025-04-06 15:04:01 -0500

    actualizacion

--- src/aplicacionNota.java
+++ src/aplicacionNota.java
@@ -1,61 +1,45 @@
 import java.util.Scanner;
-public class aplicacionNota {
+public class AplicacionNota {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        // Definición de constantes //
         final int NUM_ASIGNATURAS = 3;
-        final int NUM_ESTUDIANTES = 5 ;
+        final int NUM_ESTUDIANTES = 5;
         final double NOTA_MINIMA_APROBAR = 3.0;
 
-        // Definición de variables //
-        String [] nombresEstudiantes = new String[NUM_ESTUDIANTES];
-        String [] identificacionesEstudiantes = new String[NUM_ESTUDIANTES];
-        double [][] notasEstudiantes = new double[NUM_ESTUDIANTES][NUM_ASIGNATURAS];
-        double [] promediosEstudiantes = new double[NUM_ESTUDIANTES];
-        double [] estadoEstudiantes = new double[NUM_ESTUDIANTES];
-        double sumaNota = 0;
+        String[] nombresEstudiantes = new String[NUM_ESTUDIANTES];
+        String[] identificacionesEstudiantes = new String[NUM_ESTUDIANTES];
+        double[][] notasEstudiantes = new double[NUM_ESTUDIANTES][NUM_ASIGNATURAS];
+        double[] promediosEstudiantes = new double[NUM_ESTUDIANTES];
 
-        // Entrada de datos de los estudiantes //
-        for (int i = 0 ; i < nombresEstudiantes.length ; i++) {
+        for (int i = 0; i < NUM_ESTUDIANTES; i++) {
             System.out.println("Ingrese el nombre del estudiante " + (i + 1) + ": ");
             nombresEstudiantes[i] = input.nextLine();
-          
+
             System.out.println("Ingrese la identificación del estudiante " + (i + 1) + ": ");
             identificacionesEstudiantes[i] = input.nextLine();
-            
-            
-            for (int j = 0 ; j < NUM_ASIGNATURAS ; j++) {
-                System.out.println("Ingrese las 3 notas " + (j + 1) + " del estudiante " + (i + 1) + ": ");
+
+            double sumaNota = 0;
+            for (int j = 0; j < NUM_ASIGNATURAS; j++) {
+                System.out.println("Ingrese la nota " + (j + 1) + " del estudiante " + (i + 1) + ": ");
                 notasEstudiantes[i][j] = input.nextDouble();
                 sumaNota += notasEstudiantes[i][j];
-                
             }
-
-            // Consumir el salto de línea//
-            input.nextLine(); 
-
-            // Calcular el promedio de cada estudiante //
+            input.nextLine(); // Consumir salto de línea
             promediosEstudiantes[i] = sumaNota / NUM_ASIGNATURAS;
-
-
-
         }
-        // Reporte Final //
-        System.out.println("Reporte de estudiantes: ");
-        for (int i = 0 ; i < NUM_ESTUDIANTES; i ++) {
-            switch (estadoEstudiantes[i] >= NOTA_MINIMA_APROBAR) {
-                case "Aprobado":
-                    
-                    break;
-            
-                default:
-                    break;
+
+        System.out.println("Reporte de estudiantes:");
+        for (int i = 0; i < NUM_ESTUDIANTES; i++) {
+            System.out.println("Estudiante " + (i + 1) + ": " + nombresEstudiantes[i] + " (ID: " + identificacionesEstudiantes[i] + ") - Promedio: " + promediosEstudiantes[i]);
+            if (promediosEstudiantes[i] >= NOTA_MINIMA_APROBAR) {
+                System.out.println("Estado: Aprobado");
+            } else {
+                System.out.println("Estado: Reprobado");
             }
-            
-            }
-        
-       input.close();
+        }
+
+        input.close();
     }
-    
 }
+
